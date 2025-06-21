8d0cfa41719503aeb82664de86cfadab7fdd6043
Ashley Franck <ashley.franck@student.howest.be>
2025-06-21 11:06:37 +0200

    Add service class for account and closes #5

---
+++ BurgerShopOrdering/BurgerShopOrdering.core/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using BurgerShopOrdering.core.Entities;
+using BurgerShopOrdering.core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerShopOrdering.core.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<JwtSecurityToken> GenerateTokenAsync(ApplicationUser applicationUser);
+        Task<ResultModel<ApplicationUser>> GetUserByEmail(string email);
+    }
+}
