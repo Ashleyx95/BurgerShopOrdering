f48f86d93b72b89fb7bb9b71300ddd3e7f71842f
Ashley Franck <ashley.franck@student.howest.be>
2025-06-21 10:55:37 +0200

    Add service class for orders #5

---
+++ BurgerShopOrdering/BurgerShopOrdering.core/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,16 @@
+﻿using BurgerShopOrdering.core.Entities;
+using BurgerShopOrdering.core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerShopOrdering.core.Services.Interfaces
+{
+    public interface IOrderService<T> : ICrudService<T> where T : class
+    {
+        Task<ResultModel<IEnumerable<T>>> GetOrdersByUserAsync(ApplicationUser user);
+        Task<ResultModel<IEnumerable<T>>> GetByStatusAsync(string status);
+    }
+}
