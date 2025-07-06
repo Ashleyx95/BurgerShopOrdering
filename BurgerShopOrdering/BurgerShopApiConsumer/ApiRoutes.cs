ad59f44a02ff8eab520f3eb653feb5f5c29736bb
Ashley Franck <liekelie10@hotmail.com>
2025-07-06 20:14:44 +0200

    Add apiRoutes #3

---
+++ BurgerShopOrdering/BurgerShopApiConsumer/ApiRoutes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerShopApiConsumer
+{
+    public class ApiRoutes
+    {
+        public const string Base = "https://8d6dgmc8-7070.euw.devtunnels.ms/api";
+        public const string Products = Base + "/products/";
+        public const string Categories = Base + "/categories/";
+        public const string Orders = Base + "/orders/";
+        public const string Accounts = Base + "/accounts/";
+    }
+}
