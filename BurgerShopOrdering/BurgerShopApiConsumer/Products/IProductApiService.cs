1b39ed2711377eb82a2dc1c402ff8b83e60c7677
Ashley Franck <liekelie10@hotmail.com>
2025-07-12 14:49:16 +0200

    Add connection between web and API for products #3

---
+++ BurgerShopOrdering/BurgerShopApiConsumer/Products/IProductApiService.cs
@@ -0,0 +1,19 @@
+﻿using BurgerShopApiConsumer.Common;
+using BurgerShopApiConsumer.Products.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerShopApiConsumer.Products
+{
+    public interface IProductApiService
+    {
+        Task<ApiResponse<ProductResponseApiModel[]>> GetProductsAsync();
+        Task<ApiResponse<ProductResponseApiModel>> GetProductByIdAsync(Guid id);
+        Task<ApiResponse<object>> CreateProductAsync(ProductCreateRequestApiModel productToCreate, string token);
+        Task<ApiResponse<object>> UpdateProductAsync(ProductUpdateRequestApiModel productToUpdate, string token);
+        Task<ApiResponse<object>> DeleteProductAsync(Guid id, string token);
+    }
+}
